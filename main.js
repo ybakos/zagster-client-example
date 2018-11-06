40f586dc18ff2d4d149a607ede14b8d84b6fb251
Yong Joseph Bakos <ybakos@humanoriented.com>
2018-11-06 09:25:15 -0800

    Add simple retrieval and display of ride count data.

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
+const BASE_URL = "https://zagster-service.herokuapp.com"
+
 $(updateView)
 
 function updateView() {
-  alert('Sweet Potatoes, lotsa sweet potatoes')
+  $.getJSON(BASE_URL + "/rides/count" , updateRideCount)
+}
+
+function updateRideCount(data) {
+  numberOfRides = data.count
+  $("h2#rideCount").html(numberOfRides)
 }
